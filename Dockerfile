848915e072816b3230dffaf483bb9cb36bb9313a
eyes2near <eyes2near@gmail.com>
2023-09-04 13:04:43 +0800

    b

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,6 @@ RUN chmod +x /home/10001/start.sh
 
 USER 10001
 
+WORKDIR /home/10001
+
 CMD ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 cd /home/10001
-go run app.go
+./app
